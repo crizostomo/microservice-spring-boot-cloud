3736259741ed52f3b0e3faf517634038b351af4a
crizostomo <diogo_crizostomo@hotmail.com>
2022-06-07 21:59:47 -0300

    Using Feign REST Client for Service Invocation - V2

--- currency-conversion-service/src/main/java/com/microservices/currencyconversionservice/controller/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/microservices/currencyconversionservice/controller/CurrencyConversionController.java
@@ -1,5 +1,7 @@
 package com.microservices.currencyconversionservice.controller;
 
+import com.microservices.currencyconversionservice.proxy.CurrencyExchangeProxy;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,6 +13,9 @@ import java.util.HashMap;
 
 @RestController
 public class CurrencyConversionController {
+
+    @Autowired
+    private CurrencyExchangeProxy proxy;
 
     @GetMapping("http://localhost:8100/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
     public CurrencyConversion calculateCurrencyConversion(
@@ -29,6 +34,20 @@ public class CurrencyConversionController {
                         uriVariables);
 
         CurrencyConversion currencyConversion = responseEntity.getBody();
+
+        return new CurrencyConversion(currencyConversion.getId(), from, to,
+                quantity, currencyConversion.getConversionMultiple(),
+                BigDecimal.ONE, currencyConversion.getEnvironment());
+    }
+
+    @GetMapping("http://localhost:8100/currency-conversion-feign/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversion calculateCurrencyConversionFeign(
+            @PathVariable String from,
+            @PathVariable String to,
+            @PathVariable String quantity
+    ) {
+
+        CurrencyConversion currencyConversion = proxy.retrieveExchangeValue(from, to);
 
         return new CurrencyConversion(currencyConversion.getId(), from, to,
                 quantity, currencyConversion.getConversionMultiple(),
