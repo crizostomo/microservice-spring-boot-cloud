7382deef5330b98127b0d3233c240b9feb4e4675
crizostomo <diogo_crizostomo@hotmail.com>
2022-06-21 08:10:13 -0300

    Working in the same versions as the instructor

--- currency-exchange-service/src/main/java/com/microservices/currencyexchangeservice/repository/CurrencyExchangeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.microservices.currencyexchangeservice.repository;
-
-import com.microservices.currencyexchangeservice.controller.CurrencyExchange;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CurrencyExchangeRepository
-        extends JpaRepository<CurrencyExchange, Long> {
-    CurrencyExchange findByFromAndTo(String from, String to);
-}
--- currency-exchange-service/src/main/resources/data.sql
+++ currency-exchange-service/src/main/resources/data.sql
@@ -1,9 +1,9 @@
 insert into currency_exchange
 (id,currency_from,currency_to,conversion_multiple,environment)
-values(10001,'USD','BRL',5,'');
+values(10001,'USD','INR',65,'');
 insert into currency_exchange
 (id,currency_from,currency_to,conversion_multiple,environment)
-values(10002,'EUR','BRL',7,'');
+values(10002,'EUR','INR',75,'');
 insert into currency_exchange
 (id,currency_from,currency_to,conversion_multiple,environment)
-values(10003,'CAD','BRL',4,'');
+values(10003,'AUD','INR',25,'');
